f5b803516364455177ccddd4ea1d8804b4028292
Gaston <gaston24@outlook.com>
2020-08-09 00:40:43 -0300

    11

--- obligatorio_react/src/components/header/userLogOut/userLogOut.js
+++ obligatorio_react/src/components/header/userLogOut/userLogOut.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 class UserLogOut extends Component{
     render(){
         return(
-            <div className="d-inline-block align-top" style={font}>
+            <div className="d-inline-block align-top" style={{ fontSize : '1.25rem%' }}>
                 <p>Nombre/<a>LogOut</a></p>                
             </div>
         );
